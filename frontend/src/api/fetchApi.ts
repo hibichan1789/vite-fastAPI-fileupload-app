763661a29423ece8998212f9d7ac54be5f720125
hibiki <takechi.hibiki1234@gmail.com>
2026-02-14 22:55:49 +0900

    ファイルアップロード機能完成

--- frontend/src/api/fetchApi.ts
+++ frontend/src/api/fetchApi.ts
@@ -1,3 +1,4 @@
+// src/api/fetchAPI.ts
 import type { FileResponse } from "../interfaces/file";
 const baseApiUrl = import.meta.env.VITE_API_URL;
 const getALLUrl = `${baseApiUrl}/file/all`
@@ -8,4 +9,18 @@ export async function fetchFileAll():Promise<FileResponse[]>{
     }
     const FileResponses:FileResponse[] = await response.json()
     return FileResponses;
+}
+
+const postFileUrl = `${baseApiUrl}/file/upload`;
+export async function postFileAsync(file:File){
+    const formData = new FormData();
+    formData.append("file", file);
+    const response = await fetch(postFileUrl,{
+        method:"POST",
+        body:formData
+    });
+    if(!response.ok){
+        throw new Error("アップロードに失敗しました");
+    }
+    return await response.json();
 }
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,56 @@
-import { fetchFileAll } from "./api/fetchApi";
+// src/main.ts
+import { fetchFileAll, postFileAsync} from "./api/fetchApi";
+import { renderTable } from "./dom/render";
+import { validateInputFiles, validateFile } from "./utils/fileUtils";
 
-const files = await fetchFileAll();
-console.log(files)
+const statusPara = document.querySelector("#upload-status") as HTMLParagraphElement;
+const uploadButton = document.querySelector("#upload-button") as HTMLButtonElement;
+const inputFile = document.querySelector("#input-file") as HTMLInputElement;
+const fileArea = document.querySelector("#file-area") as HTMLDivElement;
+
+async function render(){
+  const fileResponses = await fetchFileAll();
+  renderTable(fileArea, fileResponses);
+}
+async function init(){
+  if(!statusPara||!uploadButton||!inputFile||!fileArea){
+    console.error("読み込みに失敗しました");
+    return;
+  }
+  await render();
+}
+const MaxMB = 10;
+uploadButton.addEventListener("click",async (event)=>{
+  event.preventDefault();
+  uploadButton.disabled = true;
+  statusPara.textContent = "送信中";
+  const uploadFiles = inputFile.files;
+  if(!validateInputFiles(uploadFiles)){
+    statusPara.textContent = "ファイルを選択してください";
+    console.error("ファイルを選択してください");
+    uploadButton.disabled = false;
+    return;
+  }
+  const uploadFile = (uploadFiles as FileList)[0];
+  if(!validateFile(uploadFile, MaxMB)){
+    statusPara.textContent = "ファイルが不適切です";
+    console.error("ファイルが不適切です");
+    uploadButton.disabled = false;
+    return;
+  }
+  try{
+    await postFileAsync(uploadFile);
+    statusPara.textContent = "アップロードに成功しました";
+    inputFile.value = "";
+    render();
+  }
+  catch(error){
+    statusPara.textContent = "アップロードに失敗しました";
+    console.error("アップロードに失敗しました", error);
+  }
+  finally{
+    uploadButton.disabled = false;
+  }
+});
+
+document.addEventListener("DOMContentLoaded", init);
